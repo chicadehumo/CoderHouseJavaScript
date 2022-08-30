e57a8d2655d5d8f71ff908b6a1d49f53667be842
Angélica Salinas <angelica.salinas.villa@gmail.com>
2022-08-30 16:34:08 -0500

    Desafío 2

---
+++ Entregable 02/js/main.js
@@ -0,0 +1,22 @@
+// Ciclos
+
+debugger
+
+let peso = parseInt(prompt("Ingresa tu peso en kg:"))
+let edad = parseInt(prompt("Ingresa tu edad:"))
+let altura = parseInt(prompt("Ingresa tu estatura en cm:"))
+let sexo = prompt("Ingresa el número -161 si eres mujer o 5 si eres hombre")
+
+let calculo = parseInt((((peso * 10) + (altura * 6.25)) - (edad * 5) + (sexo)))
+
+if (calculo <= 1500) {
+    console.log("Tú nivel de consumo de calorías es: " + calculo + ". Estas en el promedio.")
+} else if (calculo >= 1501) {
+    console.log("Tú nivel de consumo de calorías es: " + calculo + ". Estas por arriba del promedio.")
+} else if (calculo <= 999) {
+    console.log("Tú nivel de consumo de calorías es: " + calculo + ". Estas por debajo del promedio.")
+} else if (calculo <= 800) {
+    console.warn("Tú nivel de consumo de calorías es: " + calculo + ". Estas fuera del rango, talvés ingresaste algún dato incorrecto.")
+} else {
+    console.warn("Tienes que ingresar un números")
+}
